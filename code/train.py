75cf81c71fd4d2461ed5f5216329e7994088532f
Cong <erdongerzong@qq.com>
2018-03-12 10:25:33 +0000

    try bucketing

--- code/train.py
+++ code/train.py
@@ -13,9 +13,9 @@ parser.add_argument('--data_folder', type=str, default='data/', help='folder tha
 parser.add_argument('--vocab_size', type=int, default=2000, help='vocab_size')
 parser.add_argument('--train_size', type=int, default=1000, help='train_size')
 parser.add_argument('--dev_size', type=int, default=1000, help='dev_size')
-parser.add_argument('--emsize', type=int, default=2000, help='size of word embeddings')
-parser.add_argument('--nhid', type=int, default=50, help='number of hidden units per layer')
-parser.add_argument('--nlayers', type=int, default=1, help='number of layers')
+parser.add_argument('--num_embed', type=int, default=2000, help='size of word embeddings')
+parser.add_argument('--num_hidden', type=int, default=50, help='number of hidden units per layer')
+parser.add_argument('--num_layers', type=int, default=1, help='number of layers')
 parser.add_argument('--lr', type=float, default=0.5, help='initial learning rate')
 parser.add_argument('--anneal', type=float, default=5,
                     help='if > 0, lowers the learning rate in a harmonically \
@@ -39,49 +39,6 @@ parser.add_argument('--gcthreshold', type=float, default=0.5,
                     help='threshold for 2bit gradient compression')
 args = parser.parse_args()
 
-'''buckets for batch training '''
-buckets = [ 10, 20, 30, 40, 50, 60]
-###############################################################################
-# Load data
-###############################################################################
-'''To speed up the subsequent data flow in the RNN model,
-   pre-process the loaded data as batches.
-   This procedure is defined in the following batchify function.
-'''
-if args.cuda:
-    context = mx.gpu(0)
-else:
-    context = mx.cpu(0)
-corpus = Corpus(vocab_size=args.vocab_size, vocab_file = args.data_folder+ 'vocab.wiki.txt')
-
-'''prepare data
-'''
-train_corpus_indexing = corpus.tokenize(args.data_folder + 'wiki-train.txt', args.train_size)
-dev_corpus_indexing   = corpus.tokenize(args.data_folder + 'wiki-dev.txt', args.dev_size)
-test_corpus_indexing   = corpus.tokenize(args.data_folder + 'wiki-test.txt')
-
-# buckets = [i for i, j in enumerate(np.bincount([len(s) for s in dev_corpus_indexing]))
-#                        if j >= args.batch_size]
-#
-# buck = bisect.bisect_left(buckets, 33)
-# print(buck, len(buckets))
-train_data  = mx.rnn.BucketSentenceIter(train_corpus_indexing, args.batch_size)
-dev_data    = mx.rnn.BucketSentenceIter(dev_corpus_indexing, args.batch_size)
-test_data    = mx.rnn.BucketSentenceIter(test_corpus_indexing, args.batch_size)
-
-def batchify(data, batch_size):
-    """Reshape data into (num_example, batch_size)"""
-    nbatch = data.shape[0] // batch_size
-    data = data[:nbatch * batch_size]
-    data = data.reshape((batch_size, nbatch)).T
-    return data
-#
-# train_data = batchify(corpus.train, args_batch_size).as_in_context(context)
-# val_data = batchify(corpus.valid, args_batch_size).as_in_context(context)
-# test_data = batchify(corpus.test, args_batch_size).as_in_context(context)
-
-
-
 
 '''Train the model and evaluate on validation and testing data sets
 helper functions that will be used during model training and evaluation.'''
@@ -95,8 +52,56 @@ def eval(data_source):
     pass
 
 def train():
-    pass
+    stack = mx.rnn.SequentialRNNCell()
+    for i in range(args.num_layers):
+        stack.add(mx.rnn.LSTMCell(num_hidden=args.num_hidden, prefix='lstm_l%d_'%i))
+
+def sym_gen(seq_len):
+    data = mx.sym.Variable('data')
+    label = mx.sym.Variable('softmax_label')
+    embed = mx.sym.Embedding(data=data, input_dim=args.vocab_size,
+                             output_dim=args.num_embed, name='embed')
+
+    stack.reset()
+    outputs, states = stack.unroll(seq_len, inputs=embed, merge_outputs=True)
+
+    pred = mx.sym.Reshape(outputs, shape=(-1, args.num_hidden))
+    pred = mx.sym.FullyConnected(data=pred, num_hidden=args.num_embed, name='pred')
+
+    label = mx.sym.Reshape(label, shape=(-1,))
+    pred = mx.sym.SoftmaxOutput(data=pred, label=label, name='softmax')
+
+    return pred, ('data',), ('softmax_label',)
 
 if __name__ == '__main__':
-    train()
-    model.collect_params().load(args.save, context)
+    if args.cuda:
+        context = mx.gpu(0)
+    else:
+        context = mx.cpu(0)
+    ###############################################################################
+    # Load data
+    ###############################################################################
+    '''To speed up the subsequent data flow in the RNN model,
+       pre-process the loaded data as batches.
+       This procedure is defined in the following batchify function.
+    '''
+    corpus = Corpus(vocab_size=args.vocab_size, vocab_file = args.data_folder+ 'vocab.wiki.txt')
+
+    '''prepare data, support buckets batches
+    '''
+    # buckets for batch training
+    # buckets = [ 10, 20, 30, 40, 50, 60]
+    train_corpus_indexing = corpus.tokenize(args.data_folder + 'wiki-train.txt', args.train_size)
+    dev_corpus_indexing   = corpus.tokenize(args.data_folder + 'wiki-dev.txt', args.dev_size)
+    test_corpus_indexing   = corpus.tokenize(args.data_folder + 'wiki-test.txt')
+
+    train_data  = mx.rnn.BucketSentenceIter(train_corpus_indexing, args.batch_size)
+    dev_data    = mx.rnn.BucketSentenceIter(dev_corpus_indexing, args.batch_size)
+    test_data    = mx.rnn.BucketSentenceIter(test_corpus_indexing, args.batch_size)
+
+    '''Build the model, initialize model parameters,
+    and configure the optimization algorithms for training the RNN model.'''
+     model = mx.mod.BucketingModule(
+        sym_gen             = sym_gen,
+        default_bucket_key  = data_train.default_bucket_key,
+        context             = contexts)
